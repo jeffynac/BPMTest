bc2bf6da94fff0fd05734d116d0f4957d32ce672
arturoc <arturoc@bpmmicro.com>
2009-02-03 15:00:45 -0600

    [MksExport "COMSerializationSDK\VC++\Examples\SingleSourceFiles\FromDocumentation\MinimalSerialServer.cpp" 1.1] Initial revision

---
+++ COMSerializationSDK/VC++/Examples/SingleSourceFiles/FromDocumentation/MinimalSerialServer.cpp
@@ -0,0 +1,32 @@
+/**
+   Copyright 2009 BPM Microsystems
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+   SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+   FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <ComSerSdk/Application.h>
+#include <ComSerSdk/SerialServer.h>
+
+class CSerialDataServer : public ComSerSdk::CSerialServerBase
+{
+   HRESULT GetSerialData(BSTR, Scripting::IDictionaryPtr &)
+   {
+      return S_OK;
+   }
+};
+
+class CEssApp : public ComSerSdk::CApplicationBase
+{
+public:
+   virtual ComSerSdk::CServerPtr CreateServer()
+   {
+      return ComSerSdk::CServerPtr(new CSerialDataServer());
+   }
+};
+
+CEssApp g_oEssApp;
