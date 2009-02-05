c94586455816a3e6a2d6239392ff052372495df9
arturoc <arturoc@bpmmicro.com>
2009-02-05 13:54:02 -0600

    [MksExport "COMSerializationSDK\EssHost\EssHost.cpp" 1.3] Detect if running as admin and register ourselves to HKLM under that situation and HKCU otherwise.

--- COMSerializationSDK/EssHost/EssHost.cpp
+++ COMSerializationSDK/EssHost/EssHost.cpp
@@ -9,7 +9,7 @@
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 //                                                                                                                    //
-// $Header: //sesca/src/Winprog/COMSerializationSDK/EssHost/rcs/EssHost.cpp 1.2 2009/02/03 16:21:42 arturoc Exp $
+// $Header: //sesca/src/Winprog/COMSerializationSDK/EssHost/rcs/EssHost.cpp 1.3 2009/02/05 13:54:02 arturoc Exp $
 //                                                                                                                    //
 ///////// 120 columns, Tab = Insert 3 Spaces ///////////////////////////////////////////////////////////////////////////
 
@@ -91,12 +91,77 @@
 #include <comdef.h>
 #include <winerror.h>
 #include <vector>
+#include <sstream>
 
 using namespace ATL;
 
 [module(exe, uuid = "{CAACA545-BB06-4490-8611-2EA464E75D4F}", name = "ExternalSerialServer")]
 class CEssHostModule
 {
+public:
+
+   CEssHostModule();
+
+   /**
+      overrides CAtlModuleT's virtual function to customize registration
+   */
+   HRESULT RegisterServer(BOOL bRegTypeLib = FALSE, const CLSID* pCLSID = NULL) throw( );
+
+   /**
+      overrides CAtlModuleT's virtual function to customize unregistration
+   */
+   HRESULT UnregisterServer(BOOL bUnRegTypeLib, const CLSID* pCLSID = NULL) throw( );
+
+private:
+
+   /**
+      Implementation of the RegisterServer() and UnregisterServer() overrides; in other words, all those overrides
+      are defined to do here is forward to this member function.  They specify pointers to member functions
+      that we bind here to appropriate objects - it's done this way since both registration and unregistration are
+      very similar, with the only difference between them being the names of the member functions that need to be
+      called.
+      Therefore we have this member function, which defines that code only once, and is parameterized on the only
+      difference:  the member functions to execute.
+      Note that upon error detection, a dialog box is displayed to notify the user as to the nature of the error.
+      This is done since registration occurs without a way of sending BPWin detailed error information since, at this
+      point, BPWin is merely executing the .exe with /regserver or /unregserver on the command line.
+
+      @param pfnRegFunc Pointer to either IRegistrar::StringRegister or IRegistrar::StringUnregister, depending
+                        on the type of registry operation to perform.
+      @param pfnModFunc Pointer to either CAtlModuleT::RegisterServer or CAtlModuleT::UnregisterServer, depending
+                        on the type of registry operation to perform.
+      @param bRegTypeLib Whether to also register the type library - this value is merely forwarded from the argument
+                         provided by the framework.
+      @param pCLSID Just like the bRegTypeLib, this is merely forwarded from the value provided by the framework.
+      @return The value returned when pfnModFunc was invoked or, if an exception occurred, the result of handling
+              the exception.  In either case, this return value is what is returned to the framework.
+   */
+   template<class TRegistrarFunc, class TModuleFunc>
+   HRESULT RegUnReg(TRegistrarFunc pfnRegFunc, TModuleFunc pfnModFunc, BOOL bRegTypeLib, const CLSID* pCLSID) throw( );
+         
+   /**
+      Creates and returns an instance of a Registrar object (see "Creating Registrar Scripts" in MSDN).
+      Also, initializes the registrar object with the registry script substitutions we want to perform based 
+      on runtime data (path to the current module and whether to add reg info to HKLM or HKCU).
+   */
+   CComPtr<IRegistrar> PrepareRegistrar();
+
+   /**
+      Determines the current path to this executable; this is for adding it to the registry information
+      for the LocalServer32 value.
+   */
+   std::wstring GetModulePath();
+
+   std::string Narrow(std::wstring const& sSource);
+
+   /**
+      Adds a replacement key/value pair to the registrar and detects errors with that operation
+      and throws exceptions based on those errors.
+   */
+   void AddReplacement(CComPtr<IRegistrar> & spoRegistrar,
+                       const std::wstring & sKey,
+                       const std::wstring & sValue);
+
 public:
 
    int WinMain(int nShowCmd ) throw();
@@ -120,13 +185,43 @@ public:
 
    HostSpecInfo GetHostSpecInfo(const CString & sCmdLine);
    HostSpecInfo GetAndDeleteSavedHostSpecInfo();
+   CString SystemErrorDescription(DWORD dwError);
    
 private:
    static const CString k_sCmdLineKeyName;
    static const CString k_sEssHostKeyName;
+   const _bstr_t m_sRegScript;
 
    bool ConfigureSpecInfo(CString sLabel, HostSpecInfo::Type eType, const CString & sToken, HostSpecInfo & oSpecInfo);
    bool TokenMatches(const CString & sCriterion, const CString & sToken);
+
+   /**
+      Class for using RAII to free a security identifier allocated in the IsUserAdmin() function, below.
+   */
+   struct CSidFreer
+   {
+      /**
+         The security identifier to free on destruction.
+      */
+      PSID m_pSidToFree;
+
+      /**
+         On construction, specify the security identifier to free on destruction.
+      */
+      explicit CSidFreer(PSID pSidToFree):
+         m_pSidToFree(pSidToFree)
+      {}
+
+      /**
+         Frees the security identifier.
+      */
+      ~CSidFreer()
+      {
+         ::FreeSid(m_pSidToFree);
+      }
+   };
+
+   bool IsUserAdmin();
 };
 
 [
@@ -136,7 +231,7 @@ private:
 	progid("ExternalSerialServer.Host.1"),
 	version(1.0),
 	uuid("7A61987B-66D9-41c8-973B-846CC37FC1A5"),
-   registration_script(script="EssHost.rgs")
+   registration_script(script="none")
 ]
 class CEssHost : public IDispatch
 {
@@ -238,7 +333,7 @@ private:
                     "%s",
                     sName.GetString(),
                     hResult,
-                    SystemErrorDescription(hResult).GetString());
+                    _AtlModule.SystemErrorDescription(hResult).GetString());
    }
 
    void ActivateFromProgId(const CString & sProgId, HRESULT & hResult)
@@ -264,7 +359,7 @@ private:
                     sProgId.GetString(),
                     pszClsid,
                     hResult,
-                    SystemErrorDescription(hResult).GetString());
+                    _AtlModule.SystemErrorDescription(hResult).GetString());
 
       ::CoTaskMemFree(pszClsid);
    }
@@ -279,7 +374,7 @@ private:
                     "%s",
                     sClsId.GetString(),
                     hResult,
-                    SystemErrorDescription(hResult).GetString());
+                    _AtlModule.SystemErrorDescription(hResult).GetString());
       if(FAILED(hResult)) return;
 
       hResult = m_spoHosted.CoCreateInstance(oClsid);
@@ -289,30 +384,7 @@ private:
                     "%s",
                     sClsId,
                     hResult,
-                    SystemErrorDescription(hResult).GetString());
-   }
-
-   CString SystemErrorDescription(HRESULT hResult)
-   {
-       LPTSTR pszBuffer = NULL;
-       const DWORD dwResult = ::FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM   |
-                                              FORMAT_MESSAGE_IGNORE_INSERTS |
-                                              FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                                              NULL,
-                                              hResult,
-                                              LANG_NEUTRAL,
-                                              reinterpret_cast<LPTSTR>(&pszBuffer),
-                                              0,
-                                              NULL);
-
-       if(dwResult != 0)
-       {
-          const CString sLoadedMsg = pszBuffer;
-          ::LocalFree(pszBuffer);
-          return sLoadedMsg.Left(sLoadedMsg.GetLength() - 2);
-       }
-
-       return CString();
+                    _AtlModule.SystemErrorDescription(hResult).GetString());
    }
 
    CComPtr<IDispatch> m_spoHosted;
@@ -323,17 +395,24 @@ const CString CEssHostModule::k_sEssHostKeyName = "Software\\ExternalSerialServe
 
 int CEssHostModule::WinMain(int nShowCmd ) throw( )
 {
-   const CString sCmdLine = ::GetCommandLine();
-   if(HostSpecInfo::NONE != GetHostSpecInfo(sCmdLine).m_eType)
+   try
    {
-      CRegKey oCmdLineKey;
-      oCmdLineKey.Create(HKEY_CURRENT_USER, k_sEssHostKeyName);
-      oCmdLineKey.SetKeyValue(k_sCmdLineKeyName, sCmdLine);
+      const CString sCmdLine = ::GetCommandLine();
+      if(HostSpecInfo::NONE != GetHostSpecInfo(sCmdLine).m_eType)
+      {
+         CRegKey oCmdLineKey;
+         oCmdLineKey.Create(HKEY_CURRENT_USER, k_sEssHostKeyName);
+         oCmdLineKey.SetKeyValue(k_sCmdLineKeyName, sCmdLine);
+      }
+
+      return ATL::CAtlExeModuleT<CEssHostModule>::WinMain(nShowCmd);
+   }
+   catch(std::exception & oException)
+   {
+      ::MessageBox(NULL, oException.what(), "EssHost Error", MB_OK);
    }
 
-   AtlSetPerUserRegistration(true);
-   
-   return ATL::CAtlExeModuleT<CEssHostModule>::WinMain(nShowCmd);
+   return -1;
 }
 
 bool CEssHostModule::TokenMatches(const CString & sCriterion, const CString & sToken)
@@ -388,4 +467,220 @@ CEssHostModule::HostSpecInfo CEssHostModule::GetAndDeleteSavedHostSpecInfo()
    oCmdLineKey.DeleteValue(NULL);
 
    return GetHostSpecInfo(sCmdLine);
+}
+
+bool CEssHostModule::IsUserAdmin()
+{
+   SID_IDENTIFIER_AUTHORITY oNtAuthority = SECURITY_NT_AUTHORITY;
+   PSID pAdministratorsGroup = NULL;
+   if(0 == ::AllocateAndInitializeSid(&oNtAuthority,
+                                      2,
+                                      SECURITY_BUILTIN_DOMAIN_RID,
+                                      DOMAIN_ALIAS_RID_ADMINS,
+                                      0, 0, 0, 0, 0, 0,
+                                      &pAdministratorsGroup))
+   {
+      const DWORD dwLastError = ::GetLastError();
+
+      std::ostringstream oOutStrm;
+      oOutStrm << "::AllocateAndInitializeSid() failed, could not determine if this EssHost process is a member of the"
+                  " Administrators local group.  " << SystemErrorDescription(dwLastError).GetString();
+                  
+      throw std::runtime_error(oOutStrm.str());
+   }
+
+   CSidFreer oSidFreer(pAdministratorsGroup);
+
+   BOOL bIsMember = FALSE;
+   if(0 == ::CheckTokenMembership(NULL, pAdministratorsGroup, &bIsMember))
+   {
+      const DWORD dwLastError = ::GetLastError();
+
+      std::ostringstream oOutStrm;
+      oOutStrm << "::CheckTokenMembership() failed, could not determine if this EssHost process is a member of the"
+                  " Administrators local group.  " << SystemErrorDescription(dwLastError).GetString();
+                  
+      throw std::runtime_error(oOutStrm.str());
+   }
+
+   return bIsMember != FALSE;
+}
+
+CString CEssHostModule::SystemErrorDescription(DWORD dwError)
+{
+    LPTSTR pszBuffer = NULL;
+    const DWORD dwResult = ::FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM   |
+                                           FORMAT_MESSAGE_IGNORE_INSERTS |
+                                           FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                                           NULL,
+                                           dwError,
+                                           LANG_NEUTRAL,
+                                           reinterpret_cast<LPTSTR>(&pszBuffer),
+                                           0,
+                                           NULL);
+
+    if(dwResult != 0)
+    {
+       const CString sLoadedMsg = pszBuffer;
+       ::LocalFree(pszBuffer);
+       return sLoadedMsg.Left(sLoadedMsg.GetLength() - 2);
+    }
+
+    return CString();
+}
+
+CEssHostModule::CEssHostModule():
+   m_sRegScript(
+      "%ROOT%\n"
+      "{\n"
+      "   NoRemove Software\n"
+      "   {\n"
+      "      NoRemove Classes\n"
+      "      {\n"         
+      "         ExternalSerialServer.Host.1 = s 'CEssHost Object'\n"
+      "         {\n"
+      "            CLSID = s '{7A61987B-66D9-41c8-973B-846CC37FC1A5}'\n"
+      "         }\n"
+      "         ExternalSerialServer.Host = s 'CEssHost Object'\n"
+      "         {\n"
+      "            CLSID = s '{7A61987B-66D9-41c8-973B-846CC37FC1A5}'\n"
+      "            CurVer = s 'ExternalSerialServer.Host.1'\n"
+      "         }\n"
+      "         NoRemove CLSID\n"
+      "         {\n"
+      "           ForceRemove {7A61987B-66D9-41c8-973B-846CC37FC1A5} = s 'CEssHost Object'\n"
+      "            {\n"
+      "               ProgID = s 'ExternalSerialServer.Host.1'\n"
+      "               VersionIndependentProgID = s 'ExternalSerialServer.Host'\n"
+      "               ForceRemove 'Programmable'\n"
+      "               LocalServer32 = s '%MODULE%'\n"
+      "            }\n"
+      "         }\n"
+      "      }\n"
+      "   }\n"
+      "}")
+{}
+
+HRESULT CEssHostModule::RegisterServer(BOOL bRegTypeLib /*= FALSE*/, const CLSID* pCLSID/* = NULL*/) throw( )
+{
+   return RegUnReg(&IRegistrar::StringRegister, &CAtlModuleT::RegisterServer, bRegTypeLib, pCLSID);
+}
+
+HRESULT CEssHostModule::UnregisterServer(BOOL bUnRegTypeLib, const CLSID* pCLSID /*= NULL*/) throw( )
+{
+   return RegUnReg(&IRegistrar::StringUnregister, &CAtlModuleT::UnregisterServer, bUnRegTypeLib, pCLSID);
+}
+
+template<class TRegistrarFunc, class TModuleFunc>
+HRESULT CEssHostModule::RegUnReg(TRegistrarFunc pfnRegFunc,
+                                 TModuleFunc pfnModFunc,
+                                 BOOL bRegTypeLib,
+                                 const CLSID* pCLSID) throw( )
+{
+   try
+   {
+      CComPtr<IRegistrar> spoRegistrar = PrepareRegistrar();
+      const HRESULT hResult = (spoRegistrar->*pfnRegFunc)(m_sRegScript);
+      
+      if(FAILED(hResult))
+      {
+         std::ostringstream oOutStream;
+         oOutStream << "Registrar script method call failed with HRESULT " <<
+            std::hex << hResult << "h: " << SystemErrorDescription(hResult);
+         ::MessageBox(NULL, oOutStream.str().c_str(), "Registration error", MB_OK);
+      }
+
+      // Must cast to bind the member function pointer to the base class, not CEssHostModule.
+      //
+      return (static_cast<CAtlModuleT*>(this)->*pfnModFunc)(bRegTypeLib, pCLSID);
+   }
+   catch(std::exception & oException)
+   {
+      ::MessageBox(NULL, oException.what(), "Error", MB_OK);
+   }
+   return E_FAIL;
+}
+
+CComPtr<IRegistrar> CEssHostModule::PrepareRegistrar()
+{
+	CComPtr<IRegistrar> spoRegistrar;
+
+	HRESULT hResult = CoCreateInstance(CLSID_Registrar,   
+									           NULL,
+									           CLSCTX_INPROC_SERVER,
+									           IID_IRegistrar,
+									           reinterpret_cast<void**>(&spoRegistrar));
+
+	if(FAILED(hResult))
+	{
+      std::ostringstream oOutStream;
+      oOutStream << "Could not create instance of IRegistrar object to perform registration.\n"
+                    "CoCreateInstance() returned " << std::hex << hResult << "h "
+                 << SystemErrorDescription(hResult);
+      throw std::runtime_error(oOutStream.str().c_str());
+	}
+
+   AddReplacement(spoRegistrar, L"MODULE", GetModulePath());
+
+   // From "Application Compatibility: UAC: COM Per-User Configuration" in MSDN
+   //    http://msdn.microsoft.com/en-us/library/bb756926.aspx
+   //
+   //       "Beginning with Windows Vista® and Windows Server® 2008, if the integrity level of a process is higher
+   //        than Medium, the COM runtime ignores per-user COM configuration and accesses only per-machine COM
+   //        configuration. This action reduces the surface area for elevation of privilege attacks, preventing a
+   //        process with standard user privileges from configuring a COM object with arbitrary code and having this
+   //        code called from an elevated process."
+   // 
+   // Instead of bothering to additionally detect whether we're running Vista or later, we will only detect
+   // administrator, since we know that this kind of registration will also work correctly under all supported
+   // operating systems.
+   //
+   AddReplacement(spoRegistrar, L"ROOT", IsUserAdmin() ? L"HKLM" : L"HKCU");
+
+   return spoRegistrar;
+}
+
+std::wstring CEssHostModule::GetModulePath()
+{
+   std::vector<wchar_t> oModule(_MAX_PATH + 1);
+   if(0 == ::GetModuleFileNameW(_AtlBaseModule.m_hInst, &oModule.at(0), _MAX_PATH))
+	{
+      const DWORD dwLastError = ::GetLastError();
+      std::ostringstream oOutStream;
+      oOutStream << "Could not get path to executable to use in registration data.  ::GetModuleFileNameW() set last "
+                    "error to " << dwLastError << " " << SystemErrorDescription(dwLastError);
+      throw std::runtime_error(oOutStream.str().c_str());
+	}
+
+   return &oModule.at(0);
+}
+
+std::string CEssHostModule::Narrow(std::wstring const& sSource)
+{
+   typedef std::ctype<std::wstring::value_type> CType;
+   std::vector<std::string::value_type> oResult(sSource.size() + 1);
+   CType const& oCtype = std::use_facet<CType>(std::locale());
+   oCtype.narrow(sSource.data(), sSource.data() + sSource.size(), ' ', &oResult.at(0));
+   return &oResult.at(0);
+}
+
+/**
+   Adds a replacement key/value pair to the registrar and detects errors with that operation
+   and throws exceptions based on those errors.
+*/
+void CEssHostModule::AddReplacement(CComPtr<IRegistrar> & spoRegistrar,
+                    const std::wstring & sKey,
+                    const std::wstring & sValue)
+{
+   const HRESULT hResult = spoRegistrar->AddReplacement(_bstr_t(sKey.c_str()), _bstr_t(sValue.c_str()));
+
+   if(FAILED(hResult))
+   {
+      std::wostringstream oOutStrm;
+      oOutStrm << "Could not add replacement with key \"" << sKey << "\" and value \""
+               << sValue << "\" to registrar to perform registration. IRegistrar.AddReplacement() returned " <<
+               std::hex << hResult << "h " << SystemErrorDescription(hResult);
+
+      throw std::runtime_error(Narrow(oOutStrm.str()).c_str());
+   }
 }
